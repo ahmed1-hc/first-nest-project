ddd7d0423c8243141c6423a9d4f6c01acf13e28c
Ahmad <aahmed@edgelabs.uk>
2023-02-08 20:08:03 +0500

    Add User Mock Data

---
+++ src/mock/IUsers.ts
@@ -0,0 +1,29 @@
+enum Gender {
+  male = 'Male',
+  female = 'Female',
+}
+
+interface Address {
+  address: string;
+  city: string;
+  coordinates: {
+    lat: number;
+    lng: number;
+  };
+  postalCode: string;
+  state: string;
+}
+
+export interface IUsers {
+  id: number;
+  firstName: string;
+  lastName: string;
+  age: number;
+  gender: 'Male' | 'Female';
+  email: string;
+  phone: string;
+  username: string;
+  password: string;
+  image: URL;
+  address: Address;
+}
